b888e76a99f281b7ee353cdc17c18594f44d8430
SebastienMeniere <138309008+SebastienMeniere@users.noreply.github.com>
2025-07-31 15:21:09 +0200

    Update data_processing.py

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -17,9 +17,9 @@ class Data:
 
 def get_data_node_classification(dataset_name, use_validation=False):
   ### Load data and train val test split
-  graph_df = pd.read_csv('/kaggle/input/retail-data/ml_{}.csv'.format(dataset_name))
-  edge_features = np.load('/kaggle/input/retail-data/ml_{}.npy'.format(dataset_name))
-  node_features = np.load('/kaggle/input/retail-data/ml_{}_node.npy'.format(dataset_name))
+  graph_df = pd.read_csv('/kaggle/input/retail-data2/ml_{}.csv'.format(dataset_name))
+  edge_features = np.load('/kaggle/input/retail-data2/ml_{}.npy'.format(dataset_name))
+  node_features = np.load('/kaggle/input/retail-data2/ml_{}_node.npy'.format(dataset_name))
 
   val_time, test_time = list(np.quantile(graph_df.ts, [0.70, 0.85]))
 
@@ -51,9 +51,9 @@ def get_data_node_classification(dataset_name, use_validation=False):
 
 def get_data(dataset_name, different_new_nodes_between_val_and_test=False, randomize_features=False):
   ### Load data and train val test split
-  graph_df = pd.read_csv('/kaggle/input/retail-data/ml_{}.csv'.format(dataset_name))
-  edge_features = np.load('/kaggle/input/retail-data/ml_{}.npy'.format(dataset_name))
-  node_features = np.load('/kaggle/input/retail-data/ml_{}_node.npy'.format(dataset_name))
+  graph_df = pd.read_csv('/kaggle/input/retail-data2/ml_{}.csv'.format(dataset_name))
+  edge_features = np.load('/kaggle/input/retail-data2/ml_{}.npy'.format(dataset_name))
+  node_features = np.load('/kaggle/input/retail-data2/ml_{}_node.npy'.format(dataset_name))
     
   if randomize_features:
     node_features = np.random.rand(node_features.shape[0], node_features.shape[1])
